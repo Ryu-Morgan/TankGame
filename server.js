3c443445f53e30beb67d294e3eb3164bb088cb29
testris201 <bc869@drexel.edu>
2024-11-01 15:23:06 -0400

    Use a json for directions

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 let axios = require('axios');
 let express = require('express');
+let http = require('http');
+let socketIo = require('socket.io');
+
 let app = express();
+let server = http.createServer(app);
+let io = socketIo(server);
+
 app.use(express.static('public'));
 
 let port = 3000;
 let hostname = 'localhost';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('player move', (data) => {
+    console.log('Received player move:', data); // Add this line
+    io.emit('player move', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
 });
